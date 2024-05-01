766da15c0672aa8b6b17064e8a027e8727a4a314
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-05-01 19:35:27 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
 "use strict"
-
-// const x = 5;
-// const y = 2;
-// const z = 5;
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
-// console.log(x % y);
-
-// let a = 20;
-// let b = 10;
-// console.log(a * b);
-
-// console.log(x <= z)
-// console.log(x == y);
-// console.log(x == z);
-// console.log(x != z);
-// console.log(x === y);
-// console.log(x !== y);
-const a = "5";
-console.log(Number(a));
-console.log(typeof Number(a));
-
-const b = "Hello";
-console.log(Number(b))
+// const b = '5hello';
+// console.log(Number(b));
+// console.log(typeof Number(b));
 
 
 
+// console.log(Number.parseInt('20px'));
+// console.log(Number.parseInt('3.94ng'));
+// console.log(Number.parseInt('px'));
+
+// console.log(Number.parseFloat('3.12px'));
+// console.log(Number.parseFloat('20px543'));
+
+// const a = Number('20');
+// console.log(Number.isNaN(a));
+
+// const b = Number('hello');
+// console.log(Number.isNaN(b));
+
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2);
+
+ console.log((0.1*10 + 0.2*10) /10);  //не видаляти
+
+ console.log(Math.floor(2.8));
+ console.log(Math.ceil(1.3));
+ console.log(Math.round(1.7));
+ console.log(Math.max(14, 152, 3));
+ console.log(Math.min(25, 3.5, 42, 0.7));
+ console.log(Math.pow(2, 4));
+ console.log(Math.random()*(10-1) + 1);
