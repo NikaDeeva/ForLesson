d59a5835527fb20cc4fd9adf8709c83b8642e6ce
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-05-03 19:20:16 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -10,15 +10,38 @@
 // b = 5 + 5 + '5';
 // console.log(b);
 
-const c = 'Welcome to our Lesson';
-console.log(c.length);
 
-console.log('Welcome to my house'.length);
+// const c = 'Welcome to our Lesson';
+// console.log(c.length);
 
-console.log(c.toLowerCase());
-console.log(c.toUpperCase());
+// console.log('Welcome to my house'.length);
 
-console.log(c.indexOf('to'));
+// console.log(c.toLowerCase());
+// console.log(c.toUpperCase());
 
-console.log(c.includes('Welcome'));
-console.log(c.includes('welcome'));
+// console.log(c.indexOf('to'));
+
+// console.log(c.includes('Welcome'));
+// console.log(c.includes('welcome'));
+
+// console.log(c.startsWith('Wel'));
+// console.log(c.endsWith('son'));
+
+// console.log(c.padEnd(25, '!'));
+// console.log(c.padStart(22, '!'));
+
+// const s = '     Hello       ';
+// console.log(s.trim());
+
+
+const name = 'Nika';
+const age = 13;
+
+// const message = 'My name is' + ' ' + name +'. I am' + ' ' + age + + ' ' + 'years old.';
+// console.log(message);
+
+// const message = `My name is ${name} , I am ${age} years old`;
+
+const message = `Thanks to buy it, ${name}`
+console.log(message);
+
