743d1e17968a8a2deeac297526147742f7700770
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-05-10 19:30:32 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -53,17 +53,60 @@
 //     alert('Error');
 // }
 // console.log(cost);
-let sub = prompt('Your subscribe?')
-let free = 'free';
-let pro = 'pro';
-let vip = 'vip';
-if (sub === free){
-    alert('Not allowed');
-}
-else if(sub === pro){
-    alert('Allowed');
-}
-else if(sub === vip){
-    alert('Allowed');
-}
-console.log(sub);
+
+// let sub = prompt('Your subscribe?')
+// let free = 'free';
+// let pro = 'pro';
+// let vip = 'vip';
+// if (sub === free){
+//     alert('Not allowed');
+// }
+// else if(sub === pro){
+//     alert('Allowed');
+// }
+// else if(sub === vip){
+//     alert('Allowed');
+// }
+// console.log(sub);
+
+// let type;
+// const age = 19;
+// if (age >= 18){
+//     type = 'adult';
+// }
+// else{
+//     type = 'child';
+// }
+// console.log(type);
+
+// const age = 19;
+// const type = age >= 18 ? 'adult' : 'child';
+// console.log(type);
+
+// const one = 10;
+// const two = 20;
+// const bigger = one > two ? one : two;
+// let bigger;
+// if (one > two){
+//     bigger = one;
+// }
+// else{
+//     bigger = two;
+// }
+// console.log(bigger);
+
+// const input = prompt('Your number');
+// if (input > 0){
+//     console.log('1');
+// }
+// else if (input < 0){
+//     console.log('-1');
+// }
+// else{
+//     console.log('0');
+// }
+
+const a = 3;
+const b = 5;
+const result = (a + b < 4) ? 'Lower' : 'Higher';
+console.log(result); 
