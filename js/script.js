248900f9e17487f23b1a8b8017d2af54d9f64664
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-05-15 20:23:18 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,112 +1,33 @@
 "use strict"
-// let value = 'hello';
-// console.log(Boolean(value));
-
-// let ch = null;
-// console.log(Boolean(ch));
-
-
-// &&
-
-// const number = 15;
-// const result = number > 16 && number < 20;
-// console.log(result);
-
-// ||
-
-// const number = 15;
-// const result = number < 20 || number > 30;
-// console.log(result);
-
-// console.log(!0);
-// console.log(!true);
-
-// let cost = 0;
-// const sub = 'normal';
-// if (sub === 'pro'){
-//     cost = 100;
-// }
-// console.log(cost);
-
-// let cost;
-// const sub = 'normal';
-// if (sub === 'pro'){
-//     cost = 100;
-// } 
-// else {
-//     cost = 0;
-// }
-// console.log(cost);
-
 // let cost;
 // const sub = 'premium';
-// if (sub === 'normal'){
-//     cost = 0;
-// }
-// else if(sub === 'pro'){
-//     cost = 100;
-// }
-// else if(sub === 'free'){
-//     cost = 200;
-// }
-// else{
-//     alert('Error');
+// switch (sub){
+//     case 'free':
+//         cost = 0;
+//         break;
+//     case 'pro':
+//         cost = 50;
+//         break;
+//     case 'premium':
+//         cost = 200;
+//         break;
+//     default:
+//         console.log('No value');
 // }
 // console.log(cost);
 
-// let sub = prompt('Your subscribe?')
-// let free = 'free';
-// let pro = 'pro';
-// let vip = 'vip';
-// if (sub === free){
-//     alert('Not allowed');
-// }
-// else if(sub === pro){
-//     alert('Allowed');
-// }
-// else if(sub === vip){
-//     alert('Allowed');
-// }
-// console.log(sub);
-
-// let type;
-// const age = 19;
-// if (age >= 18){
-//     type = 'adult';
-// }
-// else{
-//     type = 'child';
-// }
-// console.log(type);
-
-// const age = 19;
-// const type = age >= 18 ? 'adult' : 'child';
-// console.log(type);
-
-// const one = 10;
-// const two = 20;
-// const bigger = one > two ? one : two;
-// let bigger;
-// if (one > two){
-//     bigger = one;
-// }
-// else{
-//     bigger = two;
-// }
-// console.log(bigger);
-
-// const input = prompt('Your number');
-// if (input > 0){
-//     console.log('1');
-// }
-// else if (input < 0){
-//     console.log('-1');
-// }
-// else{
-//     console.log('0');
-// }
-
-const a = 3;
-const b = 5;
-const result = (a + b < 4) ? 'Lower' : 'Higher';
-console.log(result); 
+const x1 = 10;
+const x2 = 30;
+const number = 45;
+if (number < x1){
+console.log('Num is less');
+}
+else if (number > x2){
+    console.log('Num is more');
+}
+else if (number >= x1 && number <= x2){
+    console.log('Number is in limits');
+}
+else{
+    console.log('Idk');
+}
