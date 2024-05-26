18c3fcc7d9582987907c465269411704b89a8d35
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-05-26 10:27:14 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 "use strict"
-// let number = 0;
-// while (number < 10){
-//     console.log('number:', number);
-//     number += 10;
-// }
+let number = 0;
+while (number <= 10){
+    console.log('number:', number);
+    number += 10;
+}
 // let clientCounter = 10;
 // const maxClient = 20;
 // while (clientCounter < maxClient){
