b96a80c1b9a8b9dddf40972366b450e84de8e82f
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-05-31 19:20:21 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -29,8 +29,64 @@
 // console.log(arrTwo[1]);
 // console.log(arrOne[1] + arrTwo[1]);
 
-const movies = ['Titanic', 'Jumanji', 'Star Wars', 'Spiderman', '1+1', 'Orphan'];
-movies[6] = 'Hobbit';
-console.log(movies[0]);
-console.log(movies.length);
-console.table(movies);
+// const movies = ['Titanic', 'Jumanji', 'Star Wars', 'Spiderman', '1+1', 'Orphan'];
+// movies[6] = 'Hobbit';
+// console.log(movies[0]);
+// console.log(movies.length);
+// console.table(movies);
+
+// for 
+// 
+// for (let i = 0; i < names.length; i += 1){
+//     console.log('Name:', names[i]);
+// }
+// for (const name of names){
+//     console.log(name);
+// }
+// const names = ['Alisa', 'Matvey', 'Dmytro'];
+// const nameFind = 'Matvey';
+// let message;
+// for (const name of names){
+//     if (name === nameFind){
+//         message = 'User found';
+//         break;
+//     }
+//     message = 'User not found';
+// }
+// console.log(message);
+// const numbers = [133, 2, 67, 77, 84, 12, 765, 8, 943];
+// const sum = 15;
+// for (let i = 0; i < numbers.length; i += 1){
+//     if (numbers[i] < sum){
+//         continue;
+//     }
+//     console.log(`Number is more than ${sum} - ${numbers[i]}`)
+//     }
+// const arr = [
+//     [1, 3, 5], 
+//     [200, 5, 30],
+//     [36, 98, 80]
+// ];
+// console.log(arr[0][2]);
+// console.log(arr[2][0]);
+
+// const string  = 'GoIteens';
+// for (const letters of string ){
+//     console.log(letters);
+// }
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
+// for (let i = 0; i < cart.length; i += 1){
+//     total += cart[i];
+//     console.log(total);
+// }
+
+const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+let total = 0;
+for (let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] % 2 === 0){
+        total += numbers[i];
+    }
+}
+console.log(total);
