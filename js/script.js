6f02eda9689dc608d07bb5b1ddb1219538b359b5
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-06-07 19:29:36 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -43,3 +43,31 @@
 // const names = ['Alex', 'David', 'Nika', 'Bob', 'Yana', 'Artur'];
 // console.log(names.slice(1, 3));
 // console.log(names.slice());
+
+// const names = ['Alex', 'David', 'Nika', 'Bob', 'Yana', 'Artur'];
+// const newNames = names.splice(0, 3);
+// console.log(names);
+// console.log(newNames);
+
+// const names = ['Alex', 'David', 'Nika', 'Bob', 'Yana', 'Artur'];
+// names.splice(0, 0, 'Sonia');
+// console.log(names);
+
+// const names = ['Alex', 'David', 'Nika', 'Bob', 'Yana', 'Artur'];
+// names.splice(1, 1, 'Maria');
+// console.log(names);
+// names.splice(2, 1, 'Violeta', 'Petro', 'Tom');
+// console.log(names);
+
+// const fruits = ['apple', 'orange', 'kiwi'];
+// const numbers = [1, 3, 5 ,7];
+// const arr = fruits.concat(numbers);
+// console.log(arr);
+// console.log(fruits);
+// console.log(numbers);
+
+// const message = 'Welcome to Ukraine!';
+// const messageArr = message.split('')
+// console.log(messageArr);
+// console.log(messageArr.indexOf('l'));
+// console.log(messageArr.join(''));
