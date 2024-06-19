5c88008e44e45e4a5335396e9d50ab76a5d60c8d
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-06-19 20:32:04 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,89 +1,25 @@
 "use strict"
-// const add = function(a, b, c){
+// const sum = function(a, b, c){
 //     return a + b + c;
 // }
-// const result = add(1, 3, 7);
-// console.log(result);
-// console.log(add(5, 10, 15));
 
-// const count = function(countFrom = 0, countTo = 5, step = 2){
-//     console.log(`From count = ${countFrom}. To count = ${countTo}. Step = ${stop}`);
-//     for (let i = countFrom; i <= countTo; i += step){
-//         console.log(i);
-//     }
-// };
-// count (2, 30);
+// const add = (a, b, c) => a + b + c;
 
-// const sum = function(amount, balance){
-//     if (amount === 0){
-//         console.log('Write sum more than 0');
-//     }
-//     else if(amount > balance){
-// console.log('Money aren`t enough');
-//     }
-//     else {
-//         console.log('Operation done');
-//     }
-// };
-// sum(40, 400);
-// sum(300, 1500);
-// sum(500, 350);
+// const a = x => {return x + 5};
 
-// const sum = function(amount, balance){
-//     if (amount === 0){
-//         console.log('Write sum more than 0');
-//         return;
-//     }
-//     if(amount > balance){
-//         console.log('Money aren`t enough');
-//         return
-//     }
-//     console.log('Operation done')
-// };
-// sum(300, 400);
-
-// function add (a, b, c){
-//     console.log(a + b + c);
-//     return;
+// const b = () => {
+//     console.log('Hello');
 // }
 
-// const sum = function(){
-//     let total = 0;
-//     for (const argument of arguments){
-//         total += argument;
-//     }
-//     return total;
-// };
-// console.log(sum(2, 4, 6));
-
-// const fn = function(){
-//     const arg = Array.from(arguments);
-//     console.log(arg);
-// };
-// fn(5, 9, 10);
-
-// const b = 20;
-// const add = function(){
-//     const a = 5;
-//     return a + b;
-
-// };
-// console.log(add());
-
-// const value = 20;
-// const add = function(number){
-//     const value = 10;
-//     const inner = 5;
-//     return number + value + inner;
-// };
-// console.log(value);
-// console.log(add(25));
-
-// console.log(value);
-// const value = 10;
-
-// if (true){
-//     console.log(value);
-//     var value = 5;
-//     console.log(value);
-// }
+const arr = [1, 2, 3, 4, 5];
+function sumArray(arr, callback) {
+   let sum = 0;
+   for (let i = 0; i < arr.length; i++){
+sum += callback(arr[i]);
+   }
+   return sum;
+    };
+    function cb (elements){
+        return elements;
+    }
+    console.log(sumArray(arr, cb));
