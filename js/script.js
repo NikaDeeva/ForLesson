dd2fbcfe0b5ae48e78ba1ee85b9e18197f230e37
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-06-21 17:35:23 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -11,15 +11,42 @@
 //     console.log('Hello');
 // }
 
-const arr = [1, 2, 3, 4, 5];
-function sumArray(arr, callback) {
-   let sum = 0;
-   for (let i = 0; i < arr.length; i++){
-sum += callback(arr[i]);
-   }
-   return sum;
-    };
-    function cb (elements){
-        return elements;
-    }
-    console.log(sumArray(arr, cb));
+// const arr = [1, 2, 3, 4, 5];
+// function sumArray(arr, callback) {
+//    let sum = 0;
+//    for (let i = 0; i < arr.length; i++){
+// sum += callback(arr[i]);
+//    }
+//    return sum;
+//     };
+//     function cb (elements){
+//         return elements;
+//     }
+//     console.log(sumArray(arr, cb));
+
+// const add = (...arg) => {
+//    console.log(arg);
+// }
+// add(1, 2, 3, 4, 5);
+
+// const myNewArrowFunction = (...arg) => {
+//    console.log(arg);
+// }
+// myNewArrowFunction(1, 2, 3);
+// myNewArrowFunction(100, 200, 300, 400, 500);
+// myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
+
+// const helloOne = () => 'Hello JavaScript';
+// console.log(helloOne());
+
+// const helloTwo = (name) => `Hello ${name}`;
+// console.log(helloTwo());
+
+const mul = (n, m) => {
+   return {
+      sum: n + m,
+      mult: n * m,
+      riz: n - m
+   };
+};
+console.log(mul(2, 3));
