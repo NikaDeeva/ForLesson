31b802e05fb288ebcc92c809cd760d13caa78fde
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-07-12 16:10:32 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,119 +1,75 @@
 "use strict"
-// const user = {
-//     name: 'Bob',
-//     age: 15,
-//     class: 10,
-// }
-// for (const key in user){
-//     console.log('Keys', key);
-// }
-// for (const key in user){
-//     console.log('Values', user[key]);
-// }
-// const keys = Object.keys(user);
-// console.log(keys);
-
-// const values = Object.values(user);
-// console.log(values);
-
-// const entries = Object.entries(user);
-// console.log(user);
-
 // const hotel = {
 //     name: 'Hilton',
 //     stars: 5,
-//     capacity: 350,
-// }
-// const keys = Object.keys(hotel);
-// console.log(keys);
-// for (const key of keys){
-//     console.log(hotel[key]);
-// }
+//     capacity: 250,
+// };
+// const {name, stars, capacity} = hotel;
+// console.log(name, stars, capacity);
+// console.log(hotel);
 
-// const entries = Object.entries(hotel);
-// console.log(entries);
-// for (const entry of entries){
-//     const key = entry[0];
-//     const value = entry[1];
-//     console.log(`${key}: ${value}`);
-// }
+// const {name: hotelName, stars, status: hotelStatus} = hotel;
+// console.log(hotelName, stars, hotelStatus);
 
-// const goods = {
-//     apples: 7,
-//     oranges: 10,
-//     kiwi: 5,
-//     grapes: 8,
-// }
-// const values = Object.values(goods);
-// let total = 0;
-// for (const value of values){
-//     total += value;
-// }
-// console.log(total);
+// const {name, ...rest} = hotel;
+// console.log(name);
+// console.log(rest);
 
-// const tem = [33, 30, 28];
-// const min = Math.min(tem);
-// console.log(min);
+// const options = {
+//     size: {
+//         width: 200,
+//         height: 300,
+//     },
+//     items: ['Cake', 'Donut'],
+//     extra: true,
+// };
+// const {size: {width, height}, 
+// items: [itemOne, itemTwo], 
+// title = 'Menu'
+// } = options;
+// console.log(title);
+// console.log(width);
+// console.log(itemOne);
+// console.log(itemTwo);
 
-// const minTemp = Math.min(...tem);
-// console.log(minTemp);
+// const rgb = [200, 35, 255];
+// const [red, green, blue] = rgb;
+// console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}`);
 
-// const names = ['Bogdan', 'Yulia', 'David', 'Roman', 'Sofia', 'Dmytro'];
-// const spreadNames = [...names];
-// console.log(names);
-// console.log(spreadNames);
-// console.log(spreadNames === names);
-// const sliceNames = names.slice();
-// console.log(names);
-// console.log(sliceNames);
-// const newNames = [...names, 'Artur'];
-// console.log(newNames);
-// const num = [1, 2, 3];
-// const letters = ['a', 'b', 'c'];
-// const sumArr = [...num, ...letters];
-// (sumArr);
+// let red, green, blue;
+// [red, green, blue] = rgb;
+// console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}`);
 
-// const names = ['Sara', 'Nick', 'Din', 'Alex'];
-// const newNames = [...names, 'Bob'];
-// console.log(newNames);
+// const [red, green, blue, alpha = 0.5] = rgb;
+// console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}, Alpha: ${alpha}`);
 
-// const names = ['Sara', 'Nick', 'Din', 'Alex'];
-// const num = [2, 5, 8, 4];
-// const arr = [...names, ...num];
-// console.log(arr);
+// const [red, ...colors] = rgb;
+// console.log(red);
+// console.log(colors);
 
-// const nums = [3, 5, 4, 1];
-// const newNums = [...nums, 3];
-// console.log(newNums);
+// const [, , blue] = rgb;
+// console.log(blue);
 
-// const names = ['Sara', 'Nick', 'Din', 'Alex'];
-// const newNames = [...names.slice(0, 1), 'Bob', ...names.slice(3)];
-// console.log(newNames);
+// const user = {
+//     name: 'John',
+//     age: 30,
+//     email: 'john@example.com'
+//     }
+// const {name, age, email} = user;
+// console.log(name, age, email);
 
-// const a = {
-//     x: 5,
-//     y: 10,
-// }
-// const b = {
-//     x: 20,
-//     c: 25,
-// }
-// const d = Object.assign({}, a, b);
-// console.log(d);
-
-// const d = { r: 25, ...a, t: 30, ...b};
-// console.log(d);
-
-let a = 10;
-let b = 15;
-a = 20;
-console.log(a);
-console.log(b);
-
-const c = ['Bob'];
-const d = c;
-console.log(c);
-console.log(d);
-c.push('Nick');
-console.log(c);
-console.log(d);
+const movie = {
+    title: 'The Shawshank Redemption',
+    director: {
+    name: 'Frank Darabont',
+    nationality: 'American'
+    },
+    actors: ['Tim Robbins', 'Morgan Freeman'],
+    release_year: 1994,
+    ratings: {
+    imdb: 9.3,
+    rotten_tomatoes: 90
+    }
+    }
+    const {title, director: {name, nationality}, actors: [one, two], release_year, ratings: {imdb, rotten_tomatoes}} = movie;
+    console.log(title, name, imdb, two);
