6bbd9f76c507db94c2c4117c8024d8ad9a72bb6c
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-07-17 17:41:48 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,142 +1,55 @@
-"use strict"
-// const hotel = {
-//     name: 'Hilton',
-//     stars: 5,
-//     capacity: 250,
-// };
-// const {name, stars, capacity} = hotel;
-// console.log(name, stars, capacity);
-// console.log(hotel);
+// "use strict"
+// // імперативний
+// const nums = [1, 2, 3, 4, 5, 6];
+// const filterNum = [];
+// for (let i = 0; i < nums.length; i++){
+//   if (nums[i] > 3){
+//     filterNum.push(nums[i]);
+//   }
+// }
+// console.log(filterNum);
 
-// const {name: hotelName, stars, status: hotelStatus} = hotel;
-// console.log(hotelName, stars, hotelStatus);
+// // декларативний
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const filterNumbers = numbers.filter(value => {
+//   return value > 3;
+// });
+// console.log(filterNumbers);
 
-// const {name, ...rest} = hotel;
-// console.log(name);
-// console.log(rest);
 
-// const options = {
-//     size: {
-//         width: 200,
-//         height: 300,
-//     },
-//     items: ['Cake', 'Donut'],
-//     extra: true,
-// };
-// const {size: {width, height}, 
-// items: [itemOne, itemTwo], 
-// title = 'Menu'
-// } = options;
-// console.log(title);
-// console.log(width);
-// console.log(itemOne);
-// console.log(itemTwo);
+// const a = (array, value) => {
+//   for (let i = 0; i < array.length; i++){
+//     array[i] = array[i] * value;
+//   }
+// }
+// const number = [1, 2, 3, 4, 5, 6];
+// a(number, 2);
+// console.log(number);
 
-// const rgb = [200, 35, 255];
-// const [red, green, blue] = rgb;
-// console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}`);
+// const pure = (array, value) => {
+//   const result = [];
+//   for (let i = 0; i < array.length; i++){
+//     result.push(array[i] * value);
+//   }
+//   return result;
+// }
+// const arr = [1, 2, 3, 4, 5, 6];
+// const doubleNums = pure(arr, 2);
+// console.log(arr);
+// console.log(doubleNums);
 
-// let red, green, blue;
-// [red, green, blue] = rgb;
-// console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}`);
+// const arr = [1, 2, 3, 4, 5, 6];
 
-// const [red, green, blue, alpha = 0.5] = rgb;
-// console.log(`Red: ${red}, Green: ${green}, Blue: ${blue}, Alpha: ${alpha}`);
+// arr.forEach(num => console.log(num));
+// arr.forEach((num, index) => console.log(index, num));
 
-// const [red, ...colors] = rgb;
-// console.log(red);
-// console.log(colors);
+// const doubled = arr.map(num => num * 2);
+// console.log(doubled);
 
-// const [, , blue] = rgb;
-// console.log(blue);
-
-// const user = {
-//     name: 'John',
-//     age: 30,
-//     email: 'john@example.com'
-//     }
-// const {name, age, email} = user;
-// console.log(name, age, email);
-
-// const movie = {
-//     title: 'The Shawshank Redemption',
-//     director: {
-//     name: 'Frank Darabont',
-//     nationality: 'American'
-//     },
-//     actors: ['Tim Robbins', 'Morgan Freeman'],
-//     release_year: 1994,
-//     ratings: {
-//     imdb: 9.3,
-//     rotten_tomatoes: 90
-//     }
-//     }
-//     const {title, director: {name, nationality}, actors: [one, two], release_year, ratings: {imdb, rotten_tomatoes}} = movie;
-//     console.log(title, name, imdb, two);
-
-// const books = {
-//     count: 3,
-//     list: [
-//     {
-//     title: 'The Great Gatsby',
-//     author: 'F. Scott Fitzgerald',
-//     year: 1925
-//     },
-//     {
-//     title: 'To Kill a Mockingbird',
-//     author: 'Harper Lee',
-//     year: 1960
-//     },
-//     {
-//     title: '1984',
-//     author: 'George Orwell',
-//     year: 1949
-//     }
-//     ]
-//     }
-    // const {count, list: [one = {titleOne, authorOne, yearOne}, two = {titleTwo, authorTwo, yearTwo}, three = {titleThree, authorThree, yearThree}]} = books;
-    // const {count, list} = books;
-    // const [one, two, three] = list;
-    // const {title: titleOne, author: authorOne, year: yearOne} = one;
-    // const {title: titleTwo, author: authorTwo, year: yearTwo} = two;
-    // const {title: titleThree, author: authorThree, year: yearThree} = three;
-    // console.log(two);
-    // console.log(authorOne);
-    // console.log(yearThree);
-    // const person = {
-    //     name: 'Nelli',
-    //     surname: 'Laroy',
-    //     age: 25
-    //     };
-    //     const personInfo = ({name, surname, age}) => {
-    //         return `My name is ${name} ${surname}. I am ${age}`;
-    //     }
-        
-    //     console.log(personInfo(person));
-
-    // const student = {
-    //     name: 'Bruce',
-    //     surname: 'Lee',
-    //     grades: [4, 5, 3]
-    //     };
-    //     const calculateAverageGrade = ({grades}) => {
-    //         const [first, second, third] = grades;
-    //         return ((first + second + third) / grades.length);
-    //     }
-    //     console.log(calculateAverageGrade(student));
-
-    const items = [
-        { name: 'Футболка', price: 250, quantity: 2 },
-        { name: 'Джинси', price: 800, quantity: 1 },
-        { name: 'Кросівки', price: 1200, quantity: 1 }
-        ];
-
-        const calculateTotalCost = (item) => {
-          let total = 0;
-          for (const {price, quantity} of item){
-            total += price * quantity;
-          }
-          return total;
-        }
-        
-        console.log(calculateTotalCost(items));
+const users = [ 
+  {name: 'Bob', age: 15},
+  {name: 'Nick', age: 27},
+  {name: 'Alex', age: 47}
+];
+const names = users.map(user => user.name);
+console.log(names);
