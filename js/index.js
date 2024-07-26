bc7c9964fdb90c71a3c986895f2033e6680b982e
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-07-26 16:05:20 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,40 +1,44 @@
 "use strict"
-//  const callback = () => console.log('Hello');
-// //  
-// function fun(callback){
-//     // ...
-//     callback();
-// }
-// fun(callback());
+// const numbers = [17, 52, 65, 41, 12, 9];
+// const sum = numbers.reduce((acc, value) => acc += value, 0);
+// console.log(sum);
 
-// const numbers = [11, 32, 31, 54, 58, 36, 57, 8];
-// const cost = numbers.map(function(number){
-//     return number * 1.6;
-// }); console.log(cost);
+const tweets = [
+    {id: '012', likes: 23, tags: ['css', 'sass']},
+    {id: '017', likes: 2, tags: ['js', 'javascript']},
+    {id: '018', likes: 87, tags: ['html', 'css']},
+    {id: '019', likes: 47, tags: ['react', 'js']},
+    {id: '024', likes: 98, tags: ['nodejs', 'js']}
+];
+// const likes = tweets.reduce((totalLikes, tweet) => totalLikes += tweet.likes, 0);
+// console.log(likes);
+// const likes = tweets => tweets.reduce((totalLikes, tweet) => totalLikes += tweet.likes, 0);
+// console.log(likes(tweets));
+// const tags = tweets.reduce((allTags, tweet) => {
+//     allTags.push(...tweet.tags);
+//     return allTags;
 
-// function allElements(arr, callback){
-// for (let i = 0; i < arr.length; i++){
-// if (!callback(arr[i])){
-//     return false;
-// }
-// }
-// return true;
-// };
-// const allEven = allElements([2, 4, 6, 8, 10], function(element){
-//     return element % 2 === 0;
-// });
-// console.log(allEven);
+// }, []); console.log(tags);
 
-function filterArray(arr, callback){
-    const result = [];
-    for (let i = 0; i < arr.length; i++){
-        if (callback(arr[i])){
-            result.push(arr[i]);
-        }
-    }
-    return result;
-};
-const nums = filterArray([3, 17, 12, 15, 25, 98, 62], function(element){
-    return element % 2 !== 0;
-});
-console.log(nums);
+// const tagsSum = tweets => tweets.reduce((allTags, tweet) => {
+//         allTags.push(...tweet.tags);
+//         return allTags;
+    
+//     }, []);
+//     const tags = tagsSum(tweets);
+//     const tagsCount = (acc, tag) => {
+//         if (!acc.hasOwenProperty(tag)){
+//             acc[tag] = 0;
+
+//         };
+//         acc[tag]++;
+//         return acc;
+//     };
+//     const totalTags = tags => tags.reduce(tagsCount, {});
+//     const tagCount = totalTags(tags);
+//     console.log(tagCount);
+
+const numbers = [5, 6, 1, 3, 4, 2];
+console.log(numbers.sort())
+const names = ['Alex', 'Nick', 'Bob', 'John'];
+console.log(names.sort());
