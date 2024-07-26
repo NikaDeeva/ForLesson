0ee9eb582ba04f146b5926bc1c588836e5c66eb9
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-07-26 17:29:37 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -38,7 +38,81 @@ const tweets = [
 //     const tagCount = totalTags(tags);
 //     console.log(tagCount);
 
-const numbers = [5, 6, 1, 3, 4, 2];
-console.log(numbers.sort())
-const names = ['Alex', 'Nick', 'Bob', 'John'];
-console.log(names.sort());
+// const numbers = [5, 6, 1, 3, 4, 2];
+// console.log(numbers.sort())
+// const names = ['Alex', 'Nick', 'Bob', 'John'];
+// console.log(names.sort());
+
+// const letters = [5, 6, 1, 3, 4, 2];
+// console.log(letters.sort());
+
+// const newLetters = letters.sort((curEl, nextEl) => {
+//     return curEl - nextEl;
+// });
+// console.log(newLetters);
+
+// const fruits = ['kiwi', 'apple', 'orange', 'banana'];
+// fruits.sort(function(a, b){
+//     return b.localeCompare(a);
+// });
+// console.log(fruits);
+
+// const copy = [...fruits];
+// console.log(copy);
+// console.log(copy === fruits)
+
+const num = [5, 6, 1, 3, 4, 2];
+// const even = numbers.filter(x => x % 2 === 0);
+// console.log(even);
+// const newNum = even.map(x => x * 3);
+// console.log(newNum);
+// const reverseNum = newNum.reverse();
+// console.log(reverseNum);
+// const result = num
+// .filter(x => x % 2 === 0)
+// .map(y => y * 3)
+// .reverse();
+// console.log(result);
+
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+//     ];
+//     players.sort((a, b) => a.timePlayed - b.timePlayed);
+//     console.log(players);
+
+    // const arr = [
+    //     { name: 'John', age: 32 },
+    //     { name: 'Jane', age: 26 },
+    //     { name: 'Mike', age: 42 },
+    //     { name: 'Emily', age: 29 }
+    //     ];
+    //     arr.sort((a, b) => {
+    //         if (a.name < b.name){
+    //             return -1;
+    //         }
+    //         else if (a.name > b.name){
+    //             return 0;
+    //         }
+    //         else{
+    //             return 0;
+    //         }
+    //     });
+    //     console.log(arr);
+
+    const products = [
+        { name: 'Product 1', price: 100, quantity: 5 },
+        { name: 'Product 2', price: 50, quantity: 10 },
+        { name: 'Product 3', price: 200, quantity: 2 },
+        { name: 'Product 4', price: 50, quantity: 5 }
+        ];
+        products.sort((a, b) => {
+            if (a.price !== b.price){
+                return b.price - a.price;
+            }
+            return a.quantity - b.quantity;
+        });
+        console.log(products);
