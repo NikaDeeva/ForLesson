5434dc351f367110a526ff4b85adb06352610c0b
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-08-09 17:21:18 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -4,9 +4,29 @@
 // const listItems = document.querySelectorAll('.list-items');
 // console.log(listItems);
 
-const listWidth = 
-document.querySelector('#menu');
-listWidth.style.textTransform = 'uppercase';
-listWidth.style.fontSize = '26px';
-listWidth.style.color = 'purple';
-console.log(listWidth);
+// const listWidth = 
+// document.querySelector('#menu');
+// listWidth.style.textTransform = 'uppercase';
+// listWidth.style.fontSize = '26px';
+// listWidth.style.color = 'purple';
+// console.log(listWidth);
+
+// document.parentNode();
+// document.childNodes('.menu-item');
+// document.children();
+// document.lastChild();
+// document.firstChild();
+// document.firstElementChild();
+// document.lastElementChild();
+
+// console.log(previousSibling('.btn'));
+
+const button = document.querySelector('.btn');
+button.style.backgroundColor = 'pink';
+button.style.color = 'white';
+console.log(button.style)ж
+
+element.classList.add();
+document.classList.remove('.text');
+elem.classList.replace(old, newCLass);
+elem.classList.contains('.link');
