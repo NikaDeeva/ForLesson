9640839a704d720dfd8c1c7ff5fe1aa0e32d308e
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-08-14 17:28:23 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -21,12 +21,36 @@
 
 // console.log(previousSibling('.btn'));
 
-const button = document.querySelector('.btn');
-button.style.backgroundColor = 'pink';
-button.style.color = 'white';
-console.log(button.style)
+// const button = document.querySelector('.btn');
+// button.style.backgroundColor = 'pink';
+// button.style.color = 'white';
+// console.log(button.style);
 
-element.classList.add();
-document.classList.remove('.text');
-elem.classList.replace(old, newCLass);
-elem.classList.contains('.link');
+// element.classList.add();
+// document.classList.remove('.text');
+// elem.classList.replace(old, newCLass);
+// elem.classList.contains('.link');
+
+// const title = document.createElement('h1');
+// console.log(title);
+// title.textContent = 'hello';
+// console.log(title);
+
+
+// const image = document.createElement('img');
+// console.log(image);
+// image.src = './evel.webp';
+// image.alt = 'evel';
+
+const list = document.querySelector('.username');
+
+const lastItem = document.createElement('li');
+lastItem.textContent = 'Bob';
+list.append(lastItem);
+
+const firstItem = document.createElement('li');
+firstItem.textContent = 'Alex';
+list.prepend(firstItem);
+
+const text = document.querySelector('.text');
+text.remove()
