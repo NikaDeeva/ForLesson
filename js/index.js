c89ea033679fb9c8a9328125ab2848d18385eef8
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-08-15 17:25:29 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,56 +1,18 @@
 "use strict"
-// element.querySelector(selector);
-// const item = document.querySelector('.list-item');
-// const listItems = document.querySelectorAll('.list-items');
-// console.log(listItems);
+const title = document.querySelector('.article .title');
+title.innerHTML = 'New title';
 
-// const listWidth = 
-// document.querySelector('#menu');
-// listWidth.style.textTransform = 'uppercase';
-// listWidth.style.fontSize = '26px';
-// listWidth.style.color = 'purple';
-// console.log(listWidth);
+const technos = ['HTML', 'CSS', 'JavaScript', 'React', 'Noda'];
+const list = document.querySelector('.list');
 
-// document.parentNode();
-// document.childNodes('.menu-item');
-// document.children();
-// document.lastChild();
-// document.firstChild();
-// document.firstElementChild();
-// document.lastElementChild();
-
-// console.log(previousSibling('.btn'));
-
-// const button = document.querySelector('.btn');
-// button.style.backgroundColor = 'pink';
-// button.style.color = 'white';
-// console.log(button.style);
-
-// element.classList.add();
-// document.classList.remove('.text');
-// elem.classList.replace(old, newCLass);
-// elem.classList.contains('.link');
-
-// const title = document.createElement('h1');
-// console.log(title);
-// title.textContent = 'hello';
-// console.log(title);
+const markUp = technos
+.map(techno => `<li class='list-item'>${techno}</li>`)
+.join('');
+console.log(markUp);
+list.innerHTML = markUp;
 
 
-// const image = document.createElement('img');
-// console.log(image);
-// image.src = './evel.webp';
-// image.alt = 'evel';
-
-const list = document.querySelector('.username');
-
-const lastItem = document.createElement('li');
-lastItem.textContent = 'Bob';
-list.append(lastItem);
-
-const firstItem = document.createElement('li');
-firstItem.textContent = 'Alex';
-list.prepend(firstItem);
-
-const text = document.querySelector('.text');
-text.remove()
+const saveBtn = document.querySelector('button[data-action = "save"]');
+console.log(saveBtn.dataset.action);
+const closeBtn = document.querySelector('button[data-action = "close"]');
+console.log(closeBtn.dataset.action);
