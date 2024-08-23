009b1c3e0bfa647854ec453b33de9ef6cdc32736
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-08-23 18:58:49 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,18 +1,41 @@
 "use strict"
-const title = document.querySelector('.article .title');
-title.innerHTML = 'New title';
+// const singleBtn = document.getElementById('single');
+// const handleClick = () => alert('hello');
+// singleBtn.addEventListener('click', handleClick);
 
-const technos = ['HTML', 'CSS', 'JavaScript', 'React', 'Noda'];
-const list = document.querySelector('.list');
+// const multyBtn = document.getElementById('multiple');
+// const firstCall = () => alert('First callback');
+// const secondCall = () => alert('Second callback');
+// const thirdCall = () => alert('Third callback');
+// multyBtn.addEventListener('click', firstCall);
+// multyBtn.addEventListener('click', secondCall);
+// multyBtn.addEventListener('click', thirdCall);
 
-const markUp = technos
-.map(techno => `<li class='list-item'>${techno}</li>`)
-.join('');
-console.log(markUp);
-list.innerHTML = markUp;
+// const addBtn = document.querySelector('button[data-action="add"]');
+// const removeBtn = document.querySelector('button[data-action="remove"]');
+// const btn = document.getElementById('btn');
 
+// const handleClick = () => {
+//     alert('Click event listener alert')
+// };
+// addBtn.addEventListener('click', () => {
+//     btn.addEventListener('click', handleClick);
+// });
+// removeBtn.addEventListener('click', () => {
+//     btn.removeEventListener('click', handleClick);
+// });
 
-const saveBtn = document.querySelector('button[data-action = "save"]');
-console.log(saveBtn.dataset.action);
-const closeBtn = document.querySelector('button[data-action = "close"]');
-console.log(closeBtn.dataset.action);
+// const btn = document.querySelector('#btn');
+
+// const handleClick = event =>{
+//      console.log('event:', event);
+//      console.log('event type:', event.type);
+//     };
+// btn.addEventListener('click', handleClick);
+
+function changeColor(button){
+const colors = ['red', 'skyblue', 'green', 'coral'];
+const randomCol = colors[Math.floor(Math.random() * colors.length)];
+
+button.style.backgroundColor = randomCol;
+}
