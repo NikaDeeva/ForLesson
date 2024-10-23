84505a9ac64f39a89ba2300b70116e29a023b44f
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-10-23 20:25:52 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,76 +1,15 @@
 "use strict"
-// const parent = document.getElementById('parent');
-// const child = document.getElementById('child');
-// const des = document.getElementById('des');
+// console.log(_);
+// console.log(_.sum([1 , 2 , 3 , 4 , 5]));
 
-// parent.addEventListener('click', () => {
-//   console.log('Parent click');
-// });
-// child.addEventListener('click', () => {
-//   console.log('Child click');
-// });
-// des.addEventListener('click', () => {
-//   console.log('Des click');
-// });
-
-// const parent = document.getElementById('parent');
-
-// parent.addEventListener('click', (event) => {
-//   console.log( 'target', event.target);
-//   console.log('current target', event.currentTarget);
-// });
-
-// const parent = document.getElementById('parent');
-// const child = document.getElementById('child');
-// const des = document.getElementById('des');
-
-// parent.addEventListener('click', () => {
-// alert('parent click');
-// });
-// child.addEventListener('click', () => {
-//   alert('child click');
-// });
-//  des.addEventListener('click', (event) => {
-//   event.stopPropagation();
-//     alert('des click');
-//  });
-
-// const box = document.querySelector('div.box');
-// box.addEventListener('click', function (event){
-// console.log(event.target);
-// });
-
-const palette = document.querySelector('.color-palette');
+window.addEventListener('scroll', _.throttle(() => {
+    console.log('Scroll 300mm');
+}, 300),
+);
 const output = document.querySelector('.output');
-palette.addEventListener('click', selectColor);
-function selectColor(event){
-    if (event.target.nodeName !== 'BUTTON'){
-        return;
-    }
-    const selectedColor = event.target.dataset.color;
-    output.textContent = `Selected color: ${selectedColor}`;
-    output.style.color = selectedColor;
-}
-createPaletteItems();
+let scrollCounter = 0;
 
-function createPaletteItems() {
-const items = [];
-for (let i = 0; i < 60; i++){
-    const color = getRandomHexColor();
-    let item = document.createElement('button');
-    item.type = 'button';
-    item.dataset.color = color;
-    item.style.backgroundColor = color;
-    item.classList.add('item');
-    items.push(item);
-}
-palette.append(...items);
-}
-function getRandomHexColor(){
-    const numbers = '0123456789ABCDEF';
-    let col = '#';
-    for (let i = 0; i < 6; i++){
-        col += numbers[Math.floor(Math.random() * 16)]; 
-    }
-    return col;
-}
+document.addEventListener('scroll', () => {
+    scrollCounter++;
+    output.textContent = scrollCounter;
+});
