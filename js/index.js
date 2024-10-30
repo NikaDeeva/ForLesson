b308368bd8d4da3dafa9fe0bf81e180b3b2cbdfd
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-10-30 20:13:59 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -14,26 +14,55 @@
 //     output.textContent = scrollCounter;
 // });
 
-const vanillaOutput = document.querySelector('.output.vanilla');
-const throttleOutput = document.querySelector('.output.throttle');
-const debounceOutput = document.querySelector('.output.debounce');
+// const vanillaOutput = document.querySelector('.output.vanilla');
+// const throttleOutput = document.querySelector('.output.throttle');
+// const debounceOutput = document.querySelector('.output.debounce');
 
-const eventCounter = {
-    vanilla: 0,
-    throttle: 0,
-    debounce: 0,
-};
-document.addEventListener('scroll', () => {
-eventCounter.vanilla += 1;
-vanillaOutput.textContent = eventCounter.vanilla;
-});
-document.addEventListener('scroll', _.throttle(() => {
-    eventCounter.throttle += 1;
-    throttleOutput.textContent = eventCounter.throttle;
-}, 300),
-);
-document.addEventListener('scroll', _.debounce(() => {
-    eventCounter.debounce += 1;
-    debounceOutput.textContent = eventCounter.debounce;
-}, 300),
-);
+// const eventCounter = {
+//     vanilla: 0,
+//     throttle: 0,
+//     debounce: 0,
+// };
+// document.addEventListener('scroll', () => {
+// eventCounter.vanilla += 1;
+// vanillaOutput.textContent = eventCounter.vanilla;
+// });
+// document.addEventListener('scroll', _.throttle(() => {
+//     eventCounter.throttle += 1;
+//     throttleOutput.textContent = eventCounter.throttle;
+// }, 300),
+// );
+// document.addEventListener('scroll', _.debounce(() => {
+//     eventCounter.debounce += 1;
+//     debounceOutput.textContent = eventCounter.debounce;
+// }, 300),
+// );
+
+// const input = document.querySelector('#input');
+// input.addEventListener('input', _.throttle((event) => {
+//     const text = event.target.value;
+//     console.log('Send to server:', text);
+// }, 2500)
+// );
+// function candleInput(event){
+// const text = event.target.value;
+// console.log('Send to server:', text);
+// };
+// const throttleInput = _.throttle(candleInput, 1500);
+// const input = document.querySelector('#input');
+// input.addEventListener('input', throttleInput);
+// const input = document.querySelector('#input');
+// input.addEventListener('input', _.debounce((event) => {
+//     const text = event.target.value;
+//     console.log('Send to server:', text);
+// }, 2500)
+// );
+// document.addEventListener('mousemove', _.debounce((event) => {
+//     console.log(event.clientX, event.clientY);
+// }, 100));
+const welcome = document.querySelector('#welcome');
+const name = document.querySelector('#name');
+name.addEventListener('input', _.debounce((event) => {
+    const text = event.target.value;
+    welcome.textContent = `Welcome, ${text}`;
+}, 1000));
