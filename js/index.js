e4d65457bd79db4f87c23275c6f7462967a15c96
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-11-08 19:46:20 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -1,13 +1,52 @@
 "use strict"
 // IntersectionObserver(callback, options)
 
-const options = {
-    rootMargin: '50px',
-    threshold: 0.25,
-};
-const onEntry = (entries, observers) => {
-    entries.forEach(entry => {
-        // 
+// const options = {
+//     rootMargin: '50px',
+//     threshold: 0.25,
+// };
+// const onEntry = (entries, observers) => {
+//     entries.forEach(entry => {
+//         // 
+//     })
+// };
+// const observer = new IntersectionObserver(onEntry, options);
+
+// const options = {
+//     root: null,
+// rootMargin: '0px',
+// threshold: 0.1,
+// };
+// const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting){
+//             const img = entry.target;
+//             img.src = img.dataset.src;
+//             observer.unobserve();
+//         }
+//     })
+// }, options);
+// document.querySelectorAll('.lazy-image').forEach(img => {
+//     observer.observe(img);
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.fruits-list li');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const img = entry.target.querySelector('img');
+            const title = entry.target.querySelector('h3');
+
+            if (entry.isIntersecting){
+                img.src = img.getAttribute('data-high-res');
+                entry.target.classList.add('visible');
+            } else {
+                img.src.replace('-high', '-low');
+                entry.target.classList.remove('visible');
+            }
+        })
     })
-};
-const observer = new IntersectionObserver(onEntry, options)
+    images.forEach(image => {
+        observer.observe(image);
+    });
+});
