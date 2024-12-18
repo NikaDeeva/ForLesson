59a5089dc08b9f821a0700ad3f4afb57394b7484
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-12-18 20:22:47 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -1,34 +1,12 @@
-// const dog = {
-//     name: 'Bobik',
-//     age: 5,
-//     isGoodBoy: false,
-//     bark() {
-//         console.log('Woof');
-//     }
-// }
-// const json = JSON.stringify(dog);
-// console.log(json);
-
-// const jsonNew = JSON.stringify(() => {
-//     console.log('Hello');
-// });
-// console.log(jsonNew);
-// console.log(JSON.parse("5"));
-// console.log(JSON.parse("true"));
-
-// const jsonTwo = '{"name": "Bobik","age": 5, "isGoodBoy": false}';
-// const dogNew = JSON.parse(jsonTwo);
-// console.log(dogNew);
-// console.log(dogNew.name);
-
-const book = `{"name": "Eat Pray Love", "author": "Elizabeth", "year": 2006, "genre": "dramma"}`;
-console.log(JSON.parse(book));
-
-const name = prompt('Your name?');
-const age = prompt('Your age?');
-const data = {
-    name: name,
-    age: parseInt(age, 10),
+"use strict"
+localStorage.setItem(key, value);
+const settings = {
+    theme: 'dark',
+    isAutho: true,
+    options: [1, 2, 3],
 };
-const jsonData = JSON.stringify(data);
-console.log(JSON.parse(jsonData));
+localStorage.setItem('settings', JSON.stringify(settings));
+const savedSettings = localStorage.getItem('settings');
+const parsedSettings = JSON.parse(settingsFromLS);
+console.loga(parsedSettings);
+localStorage.removeItem('theme');
