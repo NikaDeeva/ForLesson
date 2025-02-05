83b200330c054396e9a3b79c40cf62992f748a1a
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-02-05 20:15:14 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -1,100 +1,84 @@
 "use strict"
-// const isActive = true;
+// const makePromise = (text, delay) => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(text), delay)
+//     })
+// }
+// const promiseOne = makePromise('promise one', 1500);
+// const promiseTwo = makePromise('promise two', 3000);
 
-// const one = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         if (isActive){
-//             resolve('Person is active');
-//         } else{
-//             reject('Person is not active');
-//         }
-//     }, 2000);
-// });
-// console.log('Before one.then()');
-// one.then(
-//     value => {
-//         console.log('oneResolve one.then()');
-//         console.log(value);
-//     },
-//     error => {
-//         console.log('oneReject, one.then()');
-//         console.log(error);
-//     }
-// );
-// console.log('After one.then()')
-
-// one
-// .then(value => {
+// Promise.all([promiseOne, promiseTwo])
+// .then((value) => {
 //     console.log(value);
 // })
+// .catch((error) => console.log('error'));
+
+// const makePromise = (text, delay) => {
+//         return new Promise(resolve => {
+//             setTimeout(() => resolve(text), delay)
+//         })
+//     }
+//     const promiseOne = makePromise('promise one', 1500);
+//     const promiseTwo = makePromise('promise two', 3000);
+    
+//     Promise.race([promiseOne, promiseTwo])
+//     .then((value) => {
+//         console.log(value);
+//     })
+//     .catch((error) => console.log('error'));
+
+// Promise.any([
+//     new Promise((resolve, reject) => setTimeout(() => reject(new Error('Oops!')), 1000)),
+//     new Promise((resolve, reject) => setTimeout(() => resolve('Great!'), 1500)),
+//     new Promise((resolve, reject) => setTimeout(() => reject(new Error('Error')), 2000))
+// ]).then(value => console.log(value))
 // .catch(error => {
 //     console.log(error);
-// })
+// });
 
-// const fetchUser = (username, onSuccess, onError) => {
-//     setTimeout(() => {
-//         const isSuccess = false;
+// new Promise(resolve => resolve('success')).then(value => console.log(value));
+// Promise.resolve('success').then(value => console.log(value));
 
-//         if(isSuccess){
-//             onSuccess('Success value');
-//         } else {
-//             onError('Error');
+// new Promise((resolve, reject) => reject(error)).catch(error => console.log(error));
+// Promise.reject(error).catch(error => console.log(error));
+
+// const makePromise = guestName => {
+//     if (guestName === '' || guestName === undefined){
+//         return {
+//             success: false,
+//             message: 'Error guest name',
 //         }
-//     }, 1500)
+//     } 
+//     return {
+//         success: true,
+//         message: `Welcome ${guestName}`,
+//     };
 // }
-// const onFetchSuccess = user => {
-//     console.log(user);
-// }
-// const onFetchError = error => {
-//     console.error(error);
-// }
-// fetchUser('Bob', onFetchSuccess, onFetchError);
+// const result = makePromise('');
 
-// const fetchUser = username => {
-//     return new Promise((resolve, reject) => {
-// console.log('Fetching ' + username);
-// setTimeout(() => {
-//     const isSuccess = true;
-//     if(isSuccess){
-//         resolve('Success value')
-//     } else{
-//         reject('Error');
+// if (result.success){
+//     console.log(result.message);
+// } else {
+//     console.error(result.message);
+// }
+
+// const makePromise = (guestName, onSuccess, onError) => {
+//     if (guestName === '' || guestName === undefined){
+//         return(onError('Error guest name'));
 //     }
-// }, 1500)
-//     })
+//     onSuccess(`Welcome ${guestName}`);
 // }
-// fetchUser('Bob')
-// .then(user => console.log(user))
-// .catch(error => console.error(error));
+// makePromise('', 
+//     success => console.log(success),
+//     error => console.log(error)
+// );
 
-// function addNums(a, b) {
-//     return new Promise((resolve, reject) => {
-//         if (typeof(a) !== 'number' || typeof(b) !== 'number'){
-//             reject('Data no value');
-//         } else{
-//             resolve(a + b);
-//         }
-//     })
-// }
-
-// addNums(25, 66)
-// .then(result => console.log(result))
-// .catch(error => console.error(error));
-
-const numbers = [2, 4, 6, 8, 10];
-function checkEven(numbers){
-    return new Promise((resolve, reject) => {
-        if (!Array.isArray(numbers) || numbers.some(num => typeof(num) !== 'number')){
-            reject('Data no value');
-        } else if(numbers.some(num => num % 2 !== 0)){
-            reject('There are onn numbers');
-        } else {
-            resolve('All nums are even', () => {
-                return numbers.reduce((acc, num) => acc + num, 0);
-            });
-        }
-    })
+const makePromise = guestName => {
+    if (guestName === '' || guestName === undefined){
+        return Promise.reject('Error guest name')
+    }
+    return Promise.resolve(`Welcome ${guestName}`);
 }
-checkEven(numbers)
-.then((message, sumNums) => console.log(message, sumNums))
+makePromise('Bob')
+.then(value => console.log(value))
 .catch(error => console.error(error));
