671ab7c5f20de1df72dcf1e6e6172296c6974cd5
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-02-08 10:54:42 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -4,45 +4,46 @@ let raceCounter = 0;
 const refs = {
     startBtn: document.querySelector('.js-start-race'),
     winnerField: document.querySelector('.js-winner'),
-    progress: document.querySelector('.js-progress'),
+    progressField: document.querySelector('.js-progress'),
     tableBody: document.querySelector('.js-table-body'),
 };
 refs.startBtn.addEventListener('click', onStart);
 function onStart(){
-    raceCounter++;
+    raceCounter += 1;
     const promises = horses.map(run);
     updateWinnerField('');
-    updateProgressField('Race has already begun');
+    updateProgressField('Race already has begun');
     determineWinner(promises);
     waitForAll(promises);
 }
-function determineWinner(promises){
-    Promise.race(promises).then(({horse, time}) => {
-        updateWinnerField(`Winner ${horse} finished in ${time}`);
-        updateResultsTable({horse, time, raceCounter});
-    })
+
+function determineWinner(horsesP){
+Promise.race(horsesP).then(({horse, time}) => {
+    updateWinnerField(`Winner ${horse} finished in ${time}`);
+    updateResultsTable((horse, time, raceCounter));
+})
 };
-function waitForAll(promises){
+function waitForAll(horsesP){
 Promise.all(promises).then(() => {
     updateProgressField('Race has finished');
 })
 };
 function updateWinnerField(message){
-    refs.winnerField.textContent = message;
+refs.winnerField.textContent = message;
 };
 function updateProgressField(message){
-    refs.progress.textContent = message;
+    refs.progressField.textContent = message;
 }
 function updateResultsTable({horse, time, raceCounter}){
     const tr = `<tr><td>${raceCounter}</td><td>${horse}</td><td>${time}</td>`;
-    refs.tableBody.insertAdjacentHTML('beforeend', tr)
+    refs.tableBody.insertAdjacentHTML('beforeend', tr);
 };
 function run(horse){
     return new Promise(resolve => {
         const time = getRandomTime(1500, 3000);
         setTimeout(() => {
             resolve({horse, time});
-        }, time)
+        }, time);
     })
 };
 function getRandomTime(min, max){
