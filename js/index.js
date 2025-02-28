53bcf7e3560c3b16431240d58219644efc3b1cf4
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-02-28 19:28:18 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -1,28 +1,48 @@
 "use strict"
 document.addEventListener('DOMContentLoaded', () => {
-const form = document.querySelector('.search');
-const card = document.querySelector('.card');
-if (!form || !card){
-  console.error('Data not found');
-  return;
-};
-form.addEventListener('submit', async (e) => {
-e.preventDefault();
-const query = form.query.value.trim().toLowerCase();
-if (!query) return;
-const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-if (response.ok){
-  const data = await response.json();
-  render(data);
-} else {
-  card.innerHTML = '<p class="error">Pokemon is not found</p>';
-}
-form.reset();
-});
-function render(pokemon){
-card.innerHTML = `  <h2 class="cardName">${pokemon.name}</h2>
-  <img class="cardImg" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-  <p class="cardData"><b>Weight: ${pokemon.weight}</b></p>
-  <p class="cardData"><b>Height: ${pokemon.height}</b></p>`;
-};
+const input = document.querySelector('#input');
+const form = document.querySelector('.form');
+const list = document.querySelector('.newsList');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const topic = input.value;
+  if (!topic){
+    alert('Write correct search');
+  } else{
+    list.innerHTML = '';
+fetchNews(topic);
+  }
+  function fetchNews(topic){
+    const apiKey = '96611445a11e473daa4019771c28da7d';
+  const data = fetch(`https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}&pageSize=10`)
+  .then(response => response.json())
+  .then(general => {
+    if (general.status == 'ok'){
+  renderNews(general.articles);
+    } else{
+      console.error(general.status);
+      alert('There is a problem');
+    }
+  })
+  .catch(error =>{
+    console.error(error);
+    alert('There is not such topic');
+  } );
+  
+  function renderNews(articles) {
+    const markUp = articles.map(article => {
+      return ` <li class="newsItem">
+      <p class="author">Author: ${article.author}</p>
+      <h3 class="title">${article.title}</h3>
+      <p class="describe">${article.description}</p>
+      <div class="imgWrap">
+        <img src="${article.urlToImage}" alt="" class="image">
+      </div>
+    </li>`;
+    });
+    list.insertAdjacentHTML('beforeend', markUp.join(''));
+  }
+  }
+  })
+  
 });
