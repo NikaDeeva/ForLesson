1ca6cd29be6f080e8025196cc63cd8b632eede49
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2025-03-21 18:33:34 +0100

    ok

--- js/index.js
+++ js/index.js
@@ -27,15 +27,27 @@
 // };
 // doUsers();
 
+// const fetchUsers = async () => {
+//   const url = 'https://jsonplaceholder.typicode.com';
+//   const first = await fetch(`${url}/users/1`);
+//   const second = await fetch(`${url}/users/2`);
+//   const third = await fetch(`${url}/users/3`);
+
+//   const firstU = await first.json();
+//   const secondU = await second.json();
+//   const thirdU = await third.json();
+//   console.log(firstU, secondU, thirdU);
+// };
+// fetchUsers();
+
 const fetchUsers = async () => {
   const url = 'https://jsonplaceholder.typicode.com';
-  const first = await fetch(`${url}/users/1`);
-  const second = await fetch(`${url}/users/2`);
-  const third = await fetch(`${url}/users/3`);
-
-  const firstU = await first.json();
-  const secondU = await second.json();
-  const thirdU = await third.json();
-  console.log(firstU, secondU, thirdU);
-};
+  const userIds = [1, 2, 3];
+  const array = userIds.map(async userId => {
+    const r = await fetch(`${url}/users/${userId}`);
+    return r.json();
+  });
+  const users = await Promise.all(array);
+  console.log(users);
+}
 fetchUsers();
